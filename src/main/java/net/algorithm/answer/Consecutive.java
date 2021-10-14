0b19de4cb7d27c67e591a16466e5b828475cc5b8
tiejiankudan <893507220@qq.com>
2021-10-14 10:35:42 +0800

    重新做了128.最长子序列，这下没有问题了

--- src/main/java/net/algorithm/answer/Consecutive.java
+++ src/main/java/net/algorithm/answer/Consecutive.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * @Author TieJianKuDan
  * @Date 2021/10/13 11:01
- * @Description 128. 最长连续序列(有问题)
+ * @Description 128. 最长连续序列
  * @Since version-1.0
  */
 public class Consecutive {
@@ -14,12 +14,16 @@ public class Consecutive {
         System.out.println(new Consecutive().longestConsecutive(nums));
     }
 
-    private HashMap<Integer, int[]> parent;
+    private HashMap<Integer, int[]> roots;
 
+    /**
+     * 使用并查集做，主要是练习并查集的使用
+     * @param nums
+     * @return
+     */
     public int longestConsecutive(int[] nums) {
         int res = 0;
         // 并查集的根节点选择为元素最小的那个
-        // 初始化各个元素的父节点
         init(nums);
         // 将 num 中所有差值为 1 的元素合并
         for (int i = 0; i < nums.length; i++) {
@@ -28,43 +32,64 @@ public class Consecutive {
         return res;
     }
 
+    /**
+     * 合并两个集合，并返回合并后的集合元素个数
+     * @param a roots 中一定存在
+     * @param b roots 不一定存在
+     * @return
+     */
     private int union(int a, int b) {
-        if (!parent.containsKey(b)) {
-            return parent.get(a)[1];
+        int res = 0;
+        int rootA = find(a);
+        if (!roots.containsKey(b)) {
+            return roots.get(rootA)[1];
         }
-        int roota = find(a);
-        int rootb = find(b);
-        if (roota >= rootb) {
-            parent.get(roota)[0] = rootb;
-            parent.get(rootb)[1] += parent.get(roota)[1];
+        int rootB = find(b);
+        if (rootA < rootB) {
+            int[] temp = roots.get(rootB);
+            temp[0] = rootA;
+            roots.get(rootA)[1] += temp[1];
+            res = roots.get(rootA)[1];
+        } else if (rootA > rootB) {
+            int[] temp = roots.get(rootA);
+            temp[0] = rootB;
+            roots.get(rootB)[1] += temp[1];
+            res = roots.get(rootB)[1];
         } else {
-            parent.get(rootb)[0] = roota;
-            parent.get(roota)[1] += parent.get(rootb)[1];
+            res = roots.get(rootA)[1];
         }
-        return Math.max(parent.get(roota)[1], parent.get(rootb)[1]);
+        return res;
     }
 
+    /**
+     * 找到 ele 元素所处树的根节点
+     * @param ele
+     * @return
+     */
     private int find(int ele) {
-
-        int father = parent.get(ele)[0];
+        int father = roots.get(ele)[0];
         if (father == ele) {
-            return ele;
+            return father;
         } else {
-            int root = find(parent.get(ele)[0]);
-            parent.get(ele)[0] = root;
-            if (father != root) {
-                parent.get(father)[1]--;
+            int root = find(father);
+            if (root != father) {
+                roots.get(father)[1] -= roots.get(ele)[1];
+                roots.get(ele)[0] = root;
             }
             return root;
         }
     }
 
+    /**
+     * 初始化集合
+     * @param nums
+     */
     private void init(int[] nums) {
         int len = nums.length;
-        parent = new HashMap<>(len);
+        roots = new HashMap<>(len);
         for (int i = 0; i < len; i++) {
-            // key, {父亲，个数}
-            parent.put(nums[i], new int[]{nums[i], 1});
+            // {根节点，以 num[i] 为根的子树元素个数}
+            roots.put(nums[i], new int[]{nums[i], 1});
         }
     }
 }

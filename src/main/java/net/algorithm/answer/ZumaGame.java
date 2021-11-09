4df58d6818758424bd714cd84d37baa533095963
tiejiankudan <893507220@qq.com>
2021-11-09 13:30:36 +0800

    488. 祖玛游戏(没做)

---
+++ src/main/java/net/algorithm/answer/ZumaGame.java
@@ -0,0 +1,21 @@
+package net.algorithm.answer;
+
+/**
+ * @Author TieJianKuDan
+ * @Date 2021/11/9 13:29
+ * @Description 488. 祖玛游戏
+ * @Since version-1.0
+ */
+public class ZumaGame {
+    public static void main(String[] args) {
+
+    }
+
+    public int findMinStep(String board, String hand) {
+        return 0;
+    }
+
+    private void bfs(String board, String hand) {
+
+    }
+}
